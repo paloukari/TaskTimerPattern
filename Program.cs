bfa582417396dd000c249ce196f89baa43cb47ed
Spyros Garyfallos <paloukari@hotmail.com>
2019-12-23 11:49:34 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTimerPattern
+{
+    class Program
+    {
+        static ILogger logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+        static async Task Main(string[] args)
+        {
+            // One second timer period
+            TimeSpan timerPeriod = new TimeSpan(0, 0, 1);
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var taskTimer = new TaskTimer(() => Process(), 
+                timerPeriod, 
+                logger,
+                cancellationTokenSource);
+            
+            // start the timer
+            taskTimer.Start();
+
+            // hold the main thread from exiting.
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+            Console.WriteLine("Canceled.");
+
+            // let it drain
+            await Task.Delay(timerPeriod);
+        }
+
+        private static void Process()
+        {
+            int i = 0;
+            if (i > 0)
+                throw new Exception();
+            logger.Information($"Time is {DateTime.Now.ToString("hh:mm:ss.fff")}. Processing..");
+            Thread.Sleep(20);
+        }
+    }
+}
---
+++ TaskTimer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace TaskTimerPattern
+{
+    public class TaskTimer
+    {
+        CancellationTokenSource cancellationTokenSource;
+        TimeSpan timerPeriod;
+        Action onElapsedCallback;
+        ILogger logger;
+        bool continueOnError;
+
+        public TaskTimer(Action onElapsedCallback,
+            TimeSpan timerPeriod,
+            ILogger logger,
+            CancellationTokenSource cancellationTokenSource,
+            bool continueOnError = true)
+        {
+            this.cancellationTokenSource = cancellationTokenSource;
+            this.timerPeriod = timerPeriod;
+            this.onElapsedCallback = onElapsedCallback;
+            this.logger = logger;
+            this.continueOnError = continueOnError;
+        }
+
+        public void Start()
+        {
+            Task elapsedTask = null;
+            elapsedTask = new Task((x) =>
+            {
+                Elapsed(elapsedTask, cancellationTokenSource);
+            }, cancellationTokenSource.Token);
+
+            HandleError(elapsedTask);
+
+            elapsedTask.Start();
+        }
+
+
+        private void Elapsed(Task task, object objParam)
+        {
+            var start = DateTime.Now;
+            var cancellationTokenSource = (CancellationTokenSource)objParam;
+            if (cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                logger.Information("A cancellation has been requested.");
+                return;
+            }
+
+            onElapsedCallback();
+
+            var delay = timerPeriod - (DateTime.Now - start);
+            if (delay.Ticks > 0)
+            {
+                logger.Information($"Waiting for {delay}");
+                task = Task.Delay(delay);
+            }
+            HandleError(task.ContinueWith(Elapsed, cancellationTokenSource));
+        }
+
+        private void HandleError(Task task)
+        {
+            task.ContinueWith((e) =>
+             {
+                 logger.Error($"Exception when running timer callback: {e.Exception}");
+                 if (!continueOnError)
+                     cancellationTokenSource.Cancel();
+                 else
+                     task.ContinueWith(Elapsed, cancellationTokenSource);
+             }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
